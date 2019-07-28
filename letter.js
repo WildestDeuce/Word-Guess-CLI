4f81324f855469da1a74b4a002943a2683534377
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-07-27 22:13:29 -0400

    set up some files now tried to code now I'm sleepy

---
+++ letter.js
@@ -0,0 +1,13 @@
+var inquirer = require('inquirer');
+
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
+
+  function Letter (string, boolean, func, function) {
--- word.js
+++ word.js
@@ -1,1 +1,7 @@
 var keys = require("./letter.js");
+var newArray = [];
+function Word (string, boolean, func, function) {
+    this.array = array;
+    this.position = position;
+    this.offense = offense;
+    this.defense = defense;
